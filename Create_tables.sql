3c0ae3d6c02bfeac3ffe5f085df0ae27c4720d37
adlai-nelson <157742056+adlai-nelson@users.noreply.github.com>
2024-03-19 15:52:58 -0400

    Update Create_tables.sql

--- Create_tables.sql
+++ Create_tables.sql
@@ -3,7 +3,7 @@
 CREATE TABLE mbta_subway_stops (
 STOPID SERIAL PRIMARY KEY -- Need to create a primary key
 STATION VARCHAR(255)
-LINE_NAME VARCHAR(255) 
+LINE_NAME VARCHAR(255) -- This will need to be normalized, as it is not atomic (sometimes more than one line)
 TERMINUS VARCHAR(255)
 ROUTE VARCHAR(255)
 GEOM GEOMETRY(point, 26986) -- NAD 83 Massachusetts Stateplane
@@ -19,27 +19,30 @@ GEOM GEOMETRY(point, 26986)
 CREATE TABLE mbta_busstop_events (
 EVENT_ID SERIAL PRIMARY KEY --
 STOP_ID INT	-- Foreign key
-MBTA_ROUTE INT -- I'm not sure wich route ID will be correct to join to the metrics data
+MBTA_ROUTE VARCHAR(255) -- MBTA route ID, acts as foreign key to reliability table
 CTPS_ROUTE INT -- 
 FOREIGN KEY (STOP_ID) REFERENCES mbta_bus_stops(STOP_ID)
+FOREIGN KEY (MBTA_ROUTE) REFERENCES bus_reliability(gtfs_route_id)
 );
 
 --Reliability Metrics
 
-
-CREATE TABLE bus_rail_metrics (
-METRIC_ID SERIAL PRIMARY KEY
-service_date DATE 
-gtfs_route_id VARCHAR(255)  
+-- Metric for bus is Headway / Schedule adherance
+CREATE TABLE bus_reliability (
+gtfs_route_id VARCHAR(255) PRIMARY KEY -- route ids for bus are characters (ex. '47', '57A')
 gtfs_route_name VARCHAR(255)
-mode_type VARCHAR(255) -- Includes 'Rail' and 'Bus'
-metric_type VARCHAR(255) -- Includes 'passenger wait time' for rail and 'headway/schedule' for bus
-otp_numerator NUMERIC -- The numerator and denominator for the given metric 
+otp_numerator NUMERIC -- The numerator and denominator the reliability metric 
 otp_denominator NUMERIC
-metric_value NUMERIC -- To be created by deviding numerator by denominator bounded between 0 and 1.
--- For 'passenger wait time' the value is the percent of customers who wait less than the scheduled time between trains
---
+metric_value NUMERIC -- To be created by deviding numerator by denominaton. Value bounded between 0 and 1
 );
 
-
+-- Metric for rail is Passenger Wait Time
+CREATE TABLE rail_reliability (
+gtfs_route_id VARCHAR(255) PRIMARY KEY -- route ids for rail are characters (ex. 'Orange')
+gtfs_route_name VARCHAR(255)
+otp_numerator NUMERIC -- The numerator and denominator the reliability metric 
+otp_denominator NUMERIC
+metric_value NUMERIC -- To be created by deviding numerator by denominaton. Value bounded between 0 and 1
+-- For 'passenger wait time' the value is the percent of customers who wait less than the scheduled time between trains
+);
 
