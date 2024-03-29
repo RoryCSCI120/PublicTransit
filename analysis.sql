31c0dc0ae05e5d7aee7139b7a340608abfc42a0f
adlai-nelson <adlai.nelson123@gmail.com>
2024-03-29 19:10:50 -0400

    updated sql files, continues analysis

--- analysis.sql
+++ analysis.sql
@@ -1,23 +1,36 @@
 /*
 Analysis
 */
------------------------------------
-SELECT rail_events.station, AVG(rail_reliability.otp_value) as mean_reliability, rail_points.geom
+------------------Mean -----------------
+SELECT rail_events.station, AVG(rail_reliability.otp_value) as mean_reliability
 FROM rail_events 
 LEFT JOIN rail_reliability 
 ON rail_events.line = rail_reliability.gtfs_route_id
-RIGHT JOIN rail_points
-ON rail_events.station = rail_points.station
 GROUP BY rail_events.station;
 
+SELECT bus_events.STOP_ID, AVG(bus_reliability.otp_value) as mean_reliability
+FROM bus_events 
+LEFT JOIN bus_reliability 
+ON bus_events.MBTA_ROUTE = bus_reliability.gtfs_route_id
+GROUP BY bus_events.STOP_ID;
 
-SELECT rail_points.station, AVG(rail_reliability.otp_value) as mean_reliability, rail_points.geom
-FROM rail_points
-LEFT JOIN rail_reliability 
-ON rail_events.line = rail_reliability.gtfs_route_id
-RIGHT JOIN rail_points
-ON rail_events.station = rail_points.station
-GROUP BY rail_events.station;
+--- Stops per income level
 
+SELECT  demographics.b19049_001, count(*) AS n_stops
+FROM grid, kioskdhd3
+WHERE st_contains(grid.geom,kioskdhd3.geom)
+GROUP BY grid.gid;
 
+-- n rail stops per income level
+SELECT demographics.b19049_001, count(rail_points.geom) AS n_stations -- median HH income data 
+FROM demographics
+LEFT JOIN rail_points ON st_contains(demographics.geom,rail_points.geom)
+GROUP BY demographics.gid
+ORDER BY n_stations DESC;
 
+-- n rail stops per population level
+SELECT demographics_population.total_popu, count(rail_points.geom) AS n_stations -- median HH income data 
+FROM demographics_population
+LEFT JOIN rail_points ON st_contains(demographics_population.geom,rail_points.geom)
+GROUP BY demographics_population.gid
+ORDER BY n_stations DESC;
