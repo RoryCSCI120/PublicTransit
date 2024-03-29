e405b1825984bd3eaf701e86143d1f80ab61cb48
Isack Walube <156945477+isackwalube@users.noreply.github.com>
2024-03-29 16:59:09 -0400

    Delete sql/Population.sql

--- sql/Population.sql
+++
@@ -1,9 +0,0 @@
---Demographics - Population Data
-CREATE TABLE demographics_population (
-POPULATIONID SERIAL PRIMARY KEY --Create a primary key
-NAME VARCHAR(255) -- Census Tract
-STATE VARCHAR(255)
-COUNTY VARCHAR(255)
-TOTAL_POPULATION NUMERIC -- Median Household Income Value
-GEOM GEOMETRY(polygon, 26986) -- NAD 83 Massachusetts Stateplane
-);
